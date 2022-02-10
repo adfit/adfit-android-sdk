528c6c4a44aa9dc3c02fc61fa3c3b511306cedd8
james.kang <james.kang@kakaocorp.com>
2022-02-10 16:13:47 +0900

    Update AdFit SDK (ver. 3.11.10)

--- app/src/main/java/com/kakao/adfit/publisher/sample/AboutActivity.kt
+++ app/src/main/java/com/kakao/adfit/publisher/sample/AboutActivity.kt
@@ -2,7 +2,7 @@ package com.kakao.adfit.publisher.sample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.kakao.adfit.AdfitSdk
+import com.kakao.adfit.AdFitSdk
 import kotlinx.android.synthetic.main.activie_about.*
 
 class AboutActivity : AppCompatActivity() {
@@ -11,7 +11,6 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activie_about)
-        adfitVersionText.text = "Publisher SDK v${AdfitSdk.SDK_VERSION}"
+        adfitVersionText.text = "Publisher SDK v${AdFitSdk.SDK_VERSION}"
     }
-
 }
